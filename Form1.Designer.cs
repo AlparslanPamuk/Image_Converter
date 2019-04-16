3c149409f2b92ffd31188e3362f600dbb864d1ab
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-04-16 17:37:30 +0300

    End
    Buttons and new features have been added.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,8 +37,24 @@
             this.btnblur = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.btninvert = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.lblBlur = new System.Windows.Forms.Label();
+            this.LblBrightness = new System.Windows.Forms.Label();
+            this.btnEmboss = new System.Windows.Forms.Button();
+            this.btnContrast = new System.Windows.Forms.Label();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.lblContrast = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trackBar4 = new System.Windows.Forms.TrackBar();
+            this.lblGamma = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
             this.SuspendLayout();
             // 
             // btnopen
@@ -65,7 +81,7 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 66);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 55);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(732, 460);
@@ -105,7 +121,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 542);
+            this.label1.Location = new System.Drawing.Point(12, 520);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(71, 17);
             this.label1.TabIndex = 5;
@@ -113,19 +129,150 @@
             // 
             // trackBar1
             // 
-            this.trackBar1.Location = new System.Drawing.Point(89, 542);
+            this.trackBar1.Location = new System.Drawing.Point(89, 520);
+            this.trackBar1.Maximum = 100;
             this.trackBar1.Name = "trackBar1";
             this.trackBar1.Size = new System.Drawing.Size(655, 56);
             this.trackBar1.TabIndex = 6;
             this.trackBar1.Value = 1;
             this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
             // 
+            // btninvert
+            // 
+            this.btninvert.Location = new System.Drawing.Point(393, 14);
+            this.btninvert.Name = "btninvert";
+            this.btninvert.Size = new System.Drawing.Size(75, 34);
+            this.btninvert.TabIndex = 7;
+            this.btninvert.Text = "Invert";
+            this.btninvert.UseVisualStyleBackColor = true;
+            this.btninvert.Click += new System.EventHandler(this.btninvert_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 559);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Brightness";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(89, 559);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(655, 56);
+            this.trackBar2.TabIndex = 9;
+            this.trackBar2.Value = 1;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            this.trackBar2.MouseCaptureChanged += new System.EventHandler(this.trackBar2_MouseCaptureChanged);
+            // 
+            // lblBlur
+            // 
+            this.lblBlur.AutoSize = true;
+            this.lblBlur.Location = new System.Drawing.Point(750, 520);
+            this.lblBlur.Name = "lblBlur";
+            this.lblBlur.Size = new System.Drawing.Size(16, 17);
+            this.lblBlur.TabIndex = 10;
+            this.lblBlur.Text = "0";
+            // 
+            // LblBrightness
+            // 
+            this.LblBrightness.AutoSize = true;
+            this.LblBrightness.Location = new System.Drawing.Point(750, 568);
+            this.LblBrightness.Name = "LblBrightness";
+            this.LblBrightness.Size = new System.Drawing.Size(16, 17);
+            this.LblBrightness.TabIndex = 11;
+            this.LblBrightness.Text = "0";
+            // 
+            // btnEmboss
+            // 
+            this.btnEmboss.Location = new System.Drawing.Point(474, 14);
+            this.btnEmboss.Name = "btnEmboss";
+            this.btnEmboss.Size = new System.Drawing.Size(75, 34);
+            this.btnEmboss.TabIndex = 12;
+            this.btnEmboss.Text = "Emboss";
+            this.btnEmboss.UseVisualStyleBackColor = true;
+            this.btnEmboss.Click += new System.EventHandler(this.btnEmboss_Click);
+            // 
+            // btnContrast
+            // 
+            this.btnContrast.AutoSize = true;
+            this.btnContrast.Location = new System.Drawing.Point(12, 598);
+            this.btnContrast.Name = "btnContrast";
+            this.btnContrast.Size = new System.Drawing.Size(61, 17);
+            this.btnContrast.TabIndex = 13;
+            this.btnContrast.Text = "Contrast";
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(93, 598);
+            this.trackBar3.Maximum = 100;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(655, 56);
+            this.trackBar3.TabIndex = 14;
+            this.trackBar3.Value = 1;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // lblContrast
+            // 
+            this.lblContrast.AutoSize = true;
+            this.lblContrast.Location = new System.Drawing.Point(750, 598);
+            this.lblContrast.Name = "lblContrast";
+            this.lblContrast.Size = new System.Drawing.Size(16, 17);
+            this.lblContrast.TabIndex = 15;
+            this.lblContrast.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 637);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 17);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Gamma";
+            // 
+            // trackBar4
+            // 
+            this.trackBar4.Location = new System.Drawing.Point(93, 637);
+            this.trackBar4.Maximum = 100;
+            this.trackBar4.Name = "trackBar4";
+            this.trackBar4.Size = new System.Drawing.Size(655, 56);
+            this.trackBar4.TabIndex = 17;
+            this.trackBar4.Value = 1;
+            this.trackBar4.Scroll += new System.EventHandler(this.trackBar4_Scroll);
+            // 
+            // lblGamma
+            // 
+            this.lblGamma.AutoSize = true;
+            this.lblGamma.Location = new System.Drawing.Point(750, 637);
+            this.lblGamma.Name = "lblGamma";
+            this.lblGamma.Size = new System.Drawing.Size(16, 17);
+            this.lblGamma.TabIndex = 18;
+            this.lblGamma.Text = "0";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(756, 615);
+            this.ClientSize = new System.Drawing.Size(781, 684);
+            this.Controls.Add(this.lblGamma);
+            this.Controls.Add(this.trackBar4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblContrast);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.btnContrast);
+            this.Controls.Add(this.btnEmboss);
+            this.Controls.Add(this.LblBrightness);
+            this.Controls.Add(this.lblBlur);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btninvert);
             this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnblur);
@@ -140,8 +287,12 @@
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -158,6 +309,19 @@
         private System.Windows.Forms.Button btnblur;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button btninvert;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label lblBlur;
+        private System.Windows.Forms.Label LblBrightness;
+        private System.Windows.Forms.Button btnEmboss;
+        private System.Windows.Forms.Label btnContrast;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Label lblContrast;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBar4;
+        private System.Windows.Forms.Label lblGamma;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,9 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing.Imaging;
+using System.Threading;
 
 namespace Image_Converter
 {
+    //http://gaffarsalkim.blogspot.com/2012/04/c-da-backgroundworker-kullanm.html
     public partial class Form1 : Form
     {
 
@@ -18,10 +20,44 @@ namespace Image_Converter
         Image file;
         Boolean opened = false;
         int BlurLevel = 1;
+        int trackValue = 0;
+        int LastCol = 0;
+        float contrast = 0;
+        float gamma = 1;
+
+
+
+        public static Bitmap AdjustBrightness(Bitmap Image, int Value)
+        {           //Taken the algorithm from https://stackoverflow.com/questions/23865511/contrast-with-color-matrix //
+
+            Bitmap TempBitmap = Image; // to store
+            float FinalValuePamuk = (float)Value / 255.0f;
+            Bitmap newBitmap = new Bitmap(TempBitmap.Width, TempBitmap.Height); // to store new updated image
+            Graphics NewGraphics = Graphics.FromImage(newBitmap);
+            float[][] FloatColorMatrix = 
+            {
+                new float[] {1,0,0,0,0},
+                new float[] {0,1,0,0,0},        //double dimension array
+                new float[] {0,0,1,0,0},
+                new float[] {0,0,0,1,0},
+                new float[] {FinalValuePamuk,FinalValuePamuk,FinalValuePamuk,1,1 }
+            };
+            ColorMatrix NewColorMatrix = new ColorMatrix(FloatColorMatrix);
+            ImageAttributes Attributes = new ImageAttributes();
+            Attributes.SetColorMatrix(NewColorMatrix);
+            NewGraphics.DrawImage(TempBitmap, new Rectangle(0, 0, TempBitmap.Width, TempBitmap.Height), 0, 0, TempBitmap.Width, TempBitmap.Height, GraphicsUnit.Pixel, Attributes);
+            Attributes.Dispose();
+            NewGraphics.Dispose();
+            return newBitmap;
+
+        }
+
 
         public Form1()
         {
+            CheckForIllegalCrossThreadCalls = false;
             InitializeComponent();
+            
         }
 
         private void btnopen_Click(object sender, EventArgs e)
@@ -87,7 +123,14 @@ namespace Image_Converter
 
         private void btnblur_Click(object sender, EventArgs e)
         {
+            //Thread th1 = new Thread(new ThreadStart(Do_blur));
+            //th1.Start();
+            Do_blur();
+           
+        }
 
+        private void Do_blur()
+        {
             for (int x = BlurLevel; x <= newBitmap.Width - BlurLevel; x++)
             {
                 for (int y = BlurLevel; y <= newBitmap.Height - BlurLevel; y++)
@@ -108,15 +151,241 @@ namespace Image_Converter
                     catch (Exception)
                     {
 
-                        
+
                     }
                 }
                 pictureBox1.Image = newBitmap;
-            }   }
-
+            }
+        }
         private void trackBar1_Scroll(object sender, EventArgs e)
         {
+            trackBar1.Value += 0;
+            lblBlur.Text = trackBar1.Value.ToString();
             BlurLevel = int.Parse(trackBar1.Value.ToString());
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            LblBrightness.Text = trackBar2.Value.ToString();
+
+            pictureBox1.Image = AdjustBrightness(newBitmap, trackBar2.Value);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Mustafa Alparslan Pamuk | Image Converter Program.";
+            trackBar4.Value += 1;
+            
+            
+
+        }
+
+        private void trackBar2_MouseCaptureChanged(object sender, EventArgs e)
+        {
+            trackValue = this.trackBar1.Value;
+        }
+
+        private void btninvert_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Started.");
+            backgroundWorker1.RunWorkerAsync();
+            //Do_invert();
+            MessageBox.Show("Done.");
+        }
+
+        private void Do_invert()
+        {
+            for (int x = 0; x < newBitmap.Width; x++)
+            {
+                for (int y = 0; y < newBitmap.Height; y++)
+                {
+                    Color Pixel = newBitmap.GetPixel(x, y);
+
+                    int Red = Pixel.R;
+                    int Green = Pixel.G;
+                    int Blue = Pixel.B;
+
+                    newBitmap.SetPixel(x, y, Color.FromArgb(255 - Red, 255 - Green, 255 - Blue));
+
+                }
+            }
+            pictureBox1.Image = newBitmap;
+        }
+
+        private void btnEmboss_Click(object sender, EventArgs e)
+        {
+            Bitmap nB = new Bitmap(newBitmap.Width, newBitmap.Height);
+
+            for(int x=1; x<=newBitmap.Width - 1; x++)
+            {
+                for(int y=1; y<= newBitmap.Height - 1; y++)
+                {
+                    nB.SetPixel(x, y, Color.DarkGray);
+                }
+            }
+
+            for(int x=1; x<=newBitmap.Width - 1; x++)
+            {
+                for(int y=1; y<= newBitmap.Height -1; y++)
+                {
+                    try
+                    {
+                        Color pixel = newBitmap.GetPixel(x, y);
+
+                        int ColVal = (pixel.R + pixel.G + pixel.B);
+
+                        if (LastCol == 0) LastCol = (pixel.R + pixel.G + pixel.B);
+
+                        int diff;
+
+                        if (ColVal > LastCol) { diff = ColVal - LastCol; } else { diff = LastCol - ColVal; }
+
+                        if(diff >100)
+                        {
+                            nB.SetPixel(x, y, Color.Gray);
+                            LastCol = ColVal;
+                        }
+                    }
+                    catch (Exception)
+                    {
+
+                            
+                    }
+                }
+            }
+
+            for (int y = 1; y <= newBitmap.Height - 1; y++)
+            {
+                for (int x = 1; x<=newBitmap.Width - 1; x++)
+                {
+                    try
+                    {
+                        Color pixel = newBitmap.GetPixel(x, y);
+
+                        int colVal = (pixel.R + pixel.G + pixel.B);
+
+                        if (LastCol == 0)
+                        {
+                            LastCol = (pixel.R + pixel.G + pixel.B);
+                        }
+                        int diff;
+
+                        if(colVal > LastCol)
+                        {
+                            diff = colVal - LastCol; } else { diff = LastCol - colVal;
+                        }
+
+                        if(diff > 100)
+                        {
+                            nB.SetPixel(x, y, Color.Gray);
+                            LastCol = colVal;
+                        }
+                    }
+                    catch (Exception)
+                    {
+
+                        
+                    }
+                }
+            }
+            for (int y=1; y<= newBitmap.Height - 1; y++)
+            {
+                for ( int x = 1; x <= newBitmap.Width - 1; x++)
+                {
+                    try
+                    {
+                        Color pixel = newBitmap.GetPixel(x, y);
+
+                        int colVal = (pixel.R + pixel.G + pixel.B);
+
+                        if(LastCol == 0)
+                        {
+                            LastCol = (pixel.R + pixel.G + pixel.B);
+
+                        }
+
+                        int diff;
+
+                        if (colVal > LastCol)
+                        {
+                            diff = colVal - LastCol;
+                        }
+                        else
+                            diff = LastCol - colVal;
+
+                        if ( diff > 100 )
+                        {
+                            nB.SetPixel(x, y, Color.Gray);
+                            LastCol = colVal;
+                        }
+                    }
+                    catch (Exception)
+                    {
+
+                      
+                    }
+                }
+            }
+            pictureBox1.Image = nB;
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            lblContrast.Text = trackBar3.Value.ToString();
+
+            contrast = 0.04f * trackBar3.Value;
+
+            Bitmap bmPamuk = new Bitmap(newBitmap.Width, newBitmap.Height);
+
+            Graphics g = Graphics.FromImage(bmPamuk);
+            ImageAttributes iaMustafaAlparslanPAmuk = new ImageAttributes();
+
+            ColorMatrix cm = new ColorMatrix(new float[][] {
+                new float[]{contrast, 0f, 0f, 0f, 0f },
+                new float[]{0f,contrast,0f,0f,0f},
+                new float[]{0f,0f,contrast,0f,0f},
+                new float[]{0f,0f,0f,1f,0f},
+                new float[]{0.001f,0.001f,0.001f,0f,1f} });
+
+            iaMustafaAlparslanPAmuk.SetColorMatrix(cm);
+
+            g.DrawImage(newBitmap, new Rectangle(0, 0, newBitmap.Width, newBitmap.Height), 0, 0, newBitmap.Width, newBitmap.Height, GraphicsUnit.Pixel, iaMustafaAlparslanPAmuk);
+            g.Dispose();
+            iaMustafaAlparslanPAmuk.Dispose();
+            pictureBox1.Image = bmPamuk;
+        }
+
+        private void trackBar4_Scroll(object sender, EventArgs e)
+        {
+            try
+            {
+                lblGamma.Text = trackBar4.Value.ToString();
+
+                gamma = 0.04f * trackBar4.Value;
+
+                Bitmap bm = new Bitmap(newBitmap.Width, newBitmap.Height);
+
+                Graphics gMustafaAlparslanPamuk = Graphics.FromImage(bm);
+                ImageAttributes iaPamuk = new ImageAttributes();
+
+                iaPamuk.SetGamma(gamma);
+
+                gMustafaAlparslanPamuk.DrawImage(newBitmap, new Rectangle(0, 0, newBitmap.Width, newBitmap.Height), 0, 0, newBitmap.Width, newBitmap.Height, GraphicsUnit.Pixel, iaPamuk);
+                gMustafaAlparslanPamuk.Dispose();
+                iaPamuk.Dispose();
+                pictureBox1.Image = bm;
+            }
+            catch (Exception )
+            {
+                MessageBox.Show("Gamma value can not be 0!","Mustafa Alparslan Pamuk | Image Converter Program",MessageBoxButtons.OK,MessageBoxIcon.Error);
+               
+            }
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Do_invert();
         }
     }
 }
