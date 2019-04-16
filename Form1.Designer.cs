0ce0ea87582a70221fbf57ab1badc2e31bb75f4e
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-04-16 10:46:31 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Image_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnopen = new System.Windows.Forms.Button();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btngreyscale = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnopen
+            // 
+            this.btnopen.Location = new System.Drawing.Point(9, 10);
+            this.btnopen.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnopen.Name = "btnopen";
+            this.btnopen.Size = new System.Drawing.Size(68, 28);
+            this.btnopen.TabIndex = 0;
+            this.btnopen.Text = "Open";
+            this.btnopen.UseVisualStyleBackColor = true;
+            this.btnopen.Click += new System.EventHandler(this.btnopen_Click);
+            // 
+            // btnsave
+            // 
+            this.btnsave.Location = new System.Drawing.Point(81, 10);
+            this.btnsave.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(68, 28);
+            this.btnsave.TabIndex = 1;
+            this.btnsave.Text = "Save";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(9, 54);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(549, 344);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "JPG|*.jpg|BMP|*.bmp|PNG|*.png|GIF|*.gif";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "JPG|*.jpg|BMP|*.bmp|PNG|*.png|GIF|*.gif";
+            // 
+            // btngreyscale
+            // 
+            this.btngreyscale.Location = new System.Drawing.Point(153, 11);
+            this.btngreyscale.Margin = new System.Windows.Forms.Padding(2);
+            this.btngreyscale.Name = "btngreyscale";
+            this.btngreyscale.Size = new System.Drawing.Size(68, 28);
+            this.btngreyscale.TabIndex = 3;
+            this.btngreyscale.Text = "Grey Scale";
+            this.btngreyscale.UseVisualStyleBackColor = true;
+            this.btngreyscale.Click += new System.EventHandler(this.btngreyscale_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(567, 408);
+            this.Controls.Add(this.btngreyscale);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.btnopen);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnopen;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btngreyscale;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace Image_Converter
+{
+    public partial class Form1 : Form
+    {
+        Bitmap newBitmap;
+        Image file;
+        Boolean opened = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnopen_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = openFileDialog1.ShowDialog();
+
+            if(dr == DialogResult.OK)
+            {
+                file = Image.FromFile(openFileDialog1.FileName);
+                newBitmap = new Bitmap(openFileDialog1.FileName);
+                pictureBox1.Image = file;
+                opened = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = saveFileDialog1.ShowDialog();
+
+            if (dr == DialogResult.OK)
+            {
+                if (opened)
+                {
+                    if (saveFileDialog1.FileName.Substring(saveFileDialog1.FileName.Length - 3).ToLower() == "bmp")
+                    {
+                        file.Save(saveFileDialog1.FileName, ImageFormat.Bmp);
+                    }
+                    if (saveFileDialog1.FileName.Substring(saveFileDialog1.FileName.Length - 3).ToLower() == "jpg")
+                    {
+                        file.Save(saveFileDialog1.FileName, ImageFormat.Jpeg);
+                    }
+                    if (saveFileDialog1.FileName.Substring(saveFileDialog1.FileName.Length - 3).ToLower() == "png")
+                    {
+                        file.Save(saveFileDialog1.FileName, ImageFormat.Png);
+                    }
+                    if (saveFileDialog1.FileName.Substring(saveFileDialog1.FileName.Length - 3).ToLower() == "gif")
+                    {
+                        file.Save(saveFileDialog1.FileName, ImageFormat.Gif);
+                    }
+                }
+                else
+                    MessageBox.Show("You need to pick a image first!", "Mustafa Alparslan Pamuk Image_Converter Program", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btngreyscale_Click(object sender, EventArgs e)
+        {
+            for (int x = 0; x<newBitmap.Width; x++)
+            {
+                for(int y= 0; y<newBitmap.Height; y++)
+                {
+                    Color originalcolorMustafaAlparslanPamuk = newBitmap.GetPixel(x, y);
+
+                    int greyscalePamuk = (int)((originalcolorMustafaAlparslanPamuk.R * .3) + (originalcolorMustafaAlparslanPamuk.G * .59) + (originalcolorMustafaAlparslanPamuk.B * .11));
+
+                    Color newColorPamuk = Color.FromArgb(greyscalePamuk, greyscalePamuk, greyscalePamuk);
+
+                    newBitmap.SetPixel(x, y, newColorPamuk);
+                }
+            }
+            pictureBox1.Image = newBitmap;
+        }
+    }
+}
